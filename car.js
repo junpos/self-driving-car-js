1c50b2b814892fecb34702a6da630ae274a19023
Jun Kim <junpos@gmail.com>
2024-02-18 23:54:17 -0500

    Add basic sensor

--- car.js
+++ car.js
@@ -11,11 +11,13 @@ class Car {
     this.friction = 0.03;
     this.angle = 0;
 
+    this.sensor = new Sensor(this);
     this.controls = new Controls();
   }
 
   update() {
     this.#move();
+    this.sensor.update();
   }
 
   #move() {
@@ -64,7 +66,7 @@ class Car {
   }
 
   draw(ctx) {
-    // ctx.save();
+    ctx.save();
     ctx.translate(this.x, this.y);
     ctx.rotate(-this.angle);
 
@@ -72,6 +74,8 @@ class Car {
     ctx.rect(-this.width / 2, -this.height / 2, this.width, this.height);
     ctx.fill();
 
-    // ctx.restore();
+    ctx.restore();
+
+    this.sensor.draw(ctx);
   }
 }
